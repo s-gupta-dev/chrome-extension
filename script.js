4ca77005f0c1ca7bff99714e059034aaaf037f21
Sucheta Gupta <sucheta.gupta.dev@gmail.com>
2025-03-25 11:08:52 -0400

    Black Out the Word Coronavirus

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function replaceText(element) {
     element.childNodes.forEach(replaceText);
   } else if (element.nodeType === Text.TEXT_NODE) {
     if (element.textContent.match(/coronavirus/gi)) {
-      element.parentElement.remove();
+      const newElement = document.createElement("span");
+      newElement.innerHTML = element.textContent.replace(
+        /(coronavirus)/gi,
+        '<span style="background-color: black; color: black;">$1</span>'
+      );
+      element.replaceWith(newElement);
     }
   }
 }
