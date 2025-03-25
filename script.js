0b6469f8c756ed2f327d831ac1dc4dca8ed85e2e
Sucheta Gupta <sucheta.gupta.dev@gmail.com>
2025-03-25 05:33:09 -0400

    Replace Coronavirus With Black Squares

---
+++ script.js
@@ -0,0 +1,12 @@
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    element.textContent = element.textContent.replace(
+      /coronavirus/gi,
+      "■■■■■■■■■■■"
+    );
+  }
+}
+
+replaceText(document.body);
